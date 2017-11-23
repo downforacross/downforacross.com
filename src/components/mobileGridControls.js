4574d7835518b581287de11cb49e89aebdc09848
stevenhao <stevenhao@gmail.com>
2017-11-23 03:03:57 -0500

    fix android text input

--- src/components/mobileGridControls.js
+++ src/components/mobileGridControls.js
@@ -4,12 +4,40 @@ import React from 'react';
 import GridControls from './gridControls';
 
 export default class MobileGridControls extends GridControls {
+  constructor() {
+    super();
+    this._handleMobileKeyDown = this.handleMobileKeyDown.bind(this);
+    this._handleInput = this.handleInput.bind(this);
+    this.prvInput = '';
+  }
+
+  handleMobileKeyDown(ev) {
+    if (ev.keyCode !== 229) { // skip it on android
+      this.handleKeyDown(ev);
+    }
+  }
+
+
+  // handle input -- which should only fire on android
+  handleInput(ev) {
+    const input = this.refs.textarea.value;
+    const prvInput = this.prvInput;
+    if (input.startsWith(prvInput)) {
+      if (input.length === prvInput.length + 1) {
+        const letter = input.substring(input.length - 1).toUpperCase();
+        this.typeLetter(letter, false); // no rebus for android
+      }
+    }
+    this.prvInput = input;
+  }
+
   render() {
     return (
       <div
         ref='gridControls'
         className='mobile-grid-controls'
-        onKeyDown={this.handleKeyDown.bind(this)}
+        onKeyDown={this._handleMobileKeyDown}
+        onInput={this._handleInput}
         onTouchEnd={e=> {
           e.preventDefault();
           this.refs.textarea.focus();
