fcf000517f2d410c44d8f12a1fbf9a814da05cd7
stevenhao <stevenhao@gmail.com>
2018-02-14 00:05:36 -0800

    add missing component

---
+++ src/components/RelativeTime.js
@@ -0,0 +1,12 @@
+import './css/RelativeTime.css';
+import { getTime } from '../actions';
+
+import React from 'react';
+import moment from 'moment';
+
+export default ({date}) => {
+  const now = getTime();
+  const diffStr = moment(date).from(moment(now));
+  const datestr = moment(date).format('L LTS')
+  return <span className='relative-time' title={datestr}>{ diffStr }</span>
+};
