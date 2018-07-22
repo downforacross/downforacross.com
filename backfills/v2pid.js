c3832da29147cdbd1d4076dfbc7161a6e1c807a6
stevenhao <stevenhao@gmail.com>
2018-07-22 01:17:23 -0700

    update backfill pid

--- backfills/v2pid.js
+++ backfills/v2pid.js
@@ -16,19 +16,26 @@ game[gid] {
 
 // npx babel-node backfills/v2pid.js
 import { db, disconnect } from '../src/actions'
+import _ from 'lodash';
 
 async function go() {
 
   for(let gid = 10400; gid < 10980; gid += 1) {
     const pid = (await db.ref(`/game/${gid}/pid`).once('value')).val();
-    console.log(gid, pid);
     if (!pid) {
       console.log('migrating', gid);
       const game = (await db.ref(`/game/${gid}`).once('value')).val();
+      if (!game) continue; // for some reason 10428 is missing
+      const events = game.events || game;
+      let pid = null;
+      const ev = _.values(events);
+      if (ev.length && ev[0].params.pid) {
+        pid = ev[0].params.pid;
+      }
       await db.ref(`/game/${gid}`).set({
         pid,
         events: {
-          ...game,
+          ...events,
         }
       });
     }
