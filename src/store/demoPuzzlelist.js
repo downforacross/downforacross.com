5f5586274d7fe9d41c9c2b5bec75466ea4de8355
stevenhao <stevenhao@gmail.com>
2018-08-01 21:33:38 -0700

    add demo puzzlelist

---
+++ src/store/demoPuzzlelist.js
@@ -0,0 +1,27 @@
+import Puzzlelist from './puzzlelist';
+import { db, SERVER_TIME } from './firebase';
+import _ from 'lodash';
+
+export default class DemoPuzzleList extends Puzzlelist {
+
+  getPages(pages, cbk) {
+    const type = 'Daily Puzzle';
+    const title = 'Test Puzzle';
+    const author = 'Catheryn Li';
+    const description = '';
+    const puz = {
+      type,
+      title,
+      author,
+      description,
+      private: false,
+      info: {
+        title, author, description, type
+      },
+    };
+    cbk(_.range(20).map(i => ({
+      ...puz,
+      title: `Test ${i}`,
+    })));
+  }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import { offline } from './firebase';
 import demoGame from './demoGame';
 import demoRoom from './demoRoom';
 import demoUser from './demoUser';
+import demoPuzzlelist from './demoPuzzlelist';
 import game from './game';
 import room from './room';
 import user from './user';
@@ -13,7 +14,7 @@ import { getUser as _demoGetUser } from './demoUser';
 export const GameModel = offline ? demoGame : game;
 export const RoomModel = offline ? demoRoom : room;
 export const UserModel = offline ? demoUser : user;
-export const PuzzlelistModel = puzzlelist;
+export const PuzzlelistModel = offline ? demoPuzzlelist : puzzlelist;
 export const PuzzleModel = puzzle;
 
 export const getUser = offline ? _demoGetUser : _getUser;
