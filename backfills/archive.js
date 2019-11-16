7df46dfaaa802f85b0c1c6d933f4333662b7ec18
stevenhao <steven@scaleapi.com>
2019-11-15 19:14:32 -0800

    creepy moons

--- backfills/archive.js
+++ backfills/archive.js
@@ -2,35 +2,39 @@
 import {db, disconnect} from '../src/actions';
 import _ from 'lodash';
 import AWS from 'aws-sdk';
+import Promise from 'bluebird';
 
 const credentials = new AWS.SharedIniFileCredentials({profile: 'sven'});
 AWS.config.credentials = credentials;
 
 async function go() {
-  for (let gid = 50000; gid < 50011; ++gid) {
-    const history = (await db.ref(`/game/${gid}/events`).once('value')).val();
-    if (!history) {
-      console.log('skipping', gid);
-      continue;
-    }
-    await new AWS.S3({
-      apiVersion: '2006-03-01',
-    })
-      .putObject({
-        Bucket: 'downforacross',
-        Key: `archive/game/${gid}/events`,
-        Body: JSON.stringify(history),
-        ACL: 'public-read',
+  const SIZE = 50;
+  for (let range = 1000; range < 1100; range += SIZE) {
+    await Promise.map(_.range(range, range + SIZE), async (gid) => {
+      const history = (await db.ref(`/game/${gid}/events`).once('value')).val();
+      if (!history) {
+        console.log('skipping', gid);
+        return;
+      }
+      await new AWS.S3({
+        apiVersion: '2006-03-01',
       })
-      .promise();
-    const url = `https://downforacross.s3-us-west-1.amazonaws.com/archive/game/${gid}/events`;
-    await db.ref(`/game/${gid}/archivedEvents`).set({
-      archivedAt: new Date(),
-      count: _.keys(history).length,
-      url,
+        .putObject({
+          Bucket: 'downforacross',
+          Key: `archive/game/${gid}/events`,
+          Body: JSON.stringify(history),
+          ACL: 'public-read',
+        })
+        .promise();
+      const url = `https://downforacross.s3-us-west-1.amazonaws.com/archive/game/${gid}/events`;
+      await db.ref(`/game/${gid}/archivedEvents`).set({
+        archivedAt: new Date(),
+        count: _.keys(history).length,
+        url,
+      });
+      await db.ref(`/game/${gid}/events`).remove();
+      console.log('done with', gid);
     });
-    await db.ref(`/game/${gid}/events`).remove();
-    console.log('done with', gid);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -21,38 +21,50 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import GlobalContext from './GlobalContext';
 
 import './style.css';
+import './dark.css';
 
+const Root = () => {
+  const initialValue = window.location.search.indexOf('dark') !== -1;
+  const [darkMode, setDarkMode] = React.useState(initialValue);
+  const toggleMolesterMoons = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <Router>
+      <GlobalContext.Provider value={{toggleMolesterMoons}}>
+        <div className={classnames('router-wrapper', {mobile: isMobile(), dark: darkMode})}>
+          <Route exact path="/" component={WelcomeV2} />
+          <Route exact path="/game/:gid" component={Game} />
+          <Route exact path="/room/:rid" component={Room} />
+          <Route exact path="/room/:rid/:gid" component={Room} />
+          <Route exact path="/replay/:gid" component={Replay} />
+          <Route exact path="/replay/solo/:uid/:pid" component={ReplaySolo} />
+          <Route exact path="/beta/replay/:gid" component={ReplayV2} />
+          <Route exact path="/replays/:pid" component={Replays} />
+          <Route exact path="/replays" component={Replays} />
+          <Route exact path="/game/solo/:pid" component={Solo} />
+          <Route exact path="/beta" component={WelcomeV2} />
+          <Route exact path="/beta/game/:gid" component={GameV2} />
+          <Route exact path="/beta/battle/:bid" component={Battle} />
+          <Route exact path="/beta/play/:pid" component={Play} />
+          <Route path="/puzzle/:pid" component={Solo} />
+          <Route path="/account" component={Account} />
+          <Route exact path="/compose" component={Compose} />
+          <Route exact path="/composition/:cid" component={Composition} />
+          <Route exact path="/secret_testing" component={Testing} />
+        </div>
+      </GlobalContext.Provider>
+    </Router>
+  );
+};
 /*
 ReactDOM.render(
   <h4 style={{marginLeft: 10}}>down for a maintenance</h4>,
   document.getElementById('root')
 );
 */
-ReactDOM.render(
-  <Router>
-    <div className={classnames('router-wrapper', {mobile: isMobile()})}>
-      <Route exact path="/" component={WelcomeV2} />
-      <Route exact path="/game/:gid" component={Game} />
-      <Route exact path="/room/:rid" component={Room} />
-      <Route exact path="/room/:rid/:gid" component={Room} />
-      <Route exact path="/replay/:gid" component={Replay} />
-      <Route exact path="/replay/solo/:uid/:pid" component={ReplaySolo} />
-      <Route exact path="/beta/replay/:gid" component={ReplayV2} />
-      <Route exact path="/replays/:pid" component={Replays} />
-      <Route exact path="/replays" component={Replays} />
-      <Route exact path="/game/solo/:pid" component={Solo} />
-      <Route exact path="/beta" component={WelcomeV2} />
-      <Route exact path="/beta/game/:gid" component={GameV2} />
-      <Route exact path="/beta/battle/:bid" component={Battle} />
-      <Route exact path="/beta/play/:pid" component={Play} />
-      <Route path="/puzzle/:pid" component={Solo} />
-      <Route path="/account" component={Account} />
-      <Route exact path="/compose" component={Compose} />
-      <Route exact path="/composition/:cid" component={Composition} />
-      <Route exact path="/secret_testing" component={Testing} />
-    </div>
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Root />, document.getElementById('root'));
