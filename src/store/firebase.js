30a1fb2097bb9046e76ec689fb4143e6d114b222
stevenhao <steven@scaleapi.com>
2020-04-05 14:51:18 -0700

    use dev db

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -6,14 +6,15 @@ let offline = true;
 offline = false;
 
 const config = {
-  apiKey: 'AIzaSyCe4BWm9kbjXFwlZcmq4x8DvLD3TDoinhA',
-  authDomain: 'crosswordsio.firebaseapp.com',
-  databaseURL: 'https://crosswordsio.firebaseio.com',
-  projectId: 'crosswordsio',
-  storageBucket: 'crosswordsio.appspot.com',
-  messagingSenderId: '1021412055058',
+  apiKey: 'AIzaSyC4Er27aLKgSK4u2Z8aRfD6mr8AvLPA8tA',
+  authDomain: 'dfac-fa059.firebaseapp.com',
+  databaseURL: 'https://dfac-fa059.firebaseio.com',
+  projectId: 'dfac-fa059',
+  storageBucket: 'dfac-fa059.appspot.com',
+  messagingSenderId: '132564774895',
+  appId: '1:132564774895:web:a3bf48cd38c4df81e8901a',
 };
-
+// Initialize Firebas
 firebase.initializeApp(config);
 const db = firebase.database();
 
