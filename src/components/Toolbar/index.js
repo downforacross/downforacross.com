9bf19a647c275bfd93d1f8339d20c3fe33ad4500
Ilona Brand <theilonabrand@gmail.com>
2020-04-12 23:31:56 -0400

    Add an info button with the rules for how to use the app (#86)
    * Add an info button with the rules for how to use the app
    
    * Add shortcuts, reword language, fix cursor effect

--- src/components/Toolbar/index.js
+++ src/components/Toolbar/index.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 
 import Clock from './Clock';
 import ActionMenu from './ActionMenu';
+import Popup from './Popup';
 import Flex from 'react-flexview';
 import {Link} from 'react-router-dom';
 
@@ -126,6 +127,71 @@ export default class Toolbar extends Component {
     );
   }
 
+  renderInfo() {
+    return (
+      <div className={'toolbar--info'}>
+        <Popup icon="fa-info-circle" onBlur={this.handleBlur}>
+          <h3>How to Enter Answers</h3>
+          <ul>
+            <li>
+              Click a cell once to enter an answer, and click that same cell again to switch between
+              horizontal and vertical orientations
+            </li>
+            <li>Click the clues to move the cursor directly to the cell for that answer</li>
+            <li>
+              Hold down the <code>Shift</code> key to enter multiple characters for rebus answers
+            </li>
+          </ul>
+          <h4>Keyboard Shortcuts</h4>
+          <table>
+            <tr>
+              <th>Shortcut</th>
+              <th>Description</th>
+            </tr>
+            <tr>
+              <td>Letter / Number</td>
+              <td>Fill in current cell and advance cursor to next unfilled cell in the same word, if any</td>
+            </tr>
+            <tr>
+              <td>
+                <code>.</code> (period)
+              </td>
+              <td>Toggle pencil mode on/off</td>
+            </tr>
+            <tr>
+              <td>Arrow keys</td>
+              <td>
+                Either move cursor along current orientation or change orientation without moving cursor
+              </td>
+            </tr>
+            <tr>
+              <td>Space bar</td>
+              <td>Flip orientation between down/across</td>
+            </tr>
+            <tr>
+              <td>
+                <code>[</code> and <code>]</code>
+              </td>
+              <td>Move cursor perpendicular to current orientation without changing orientation</td>
+            </tr>
+            <tr>
+              <td>
+                <code>Tab</code> and <code>Shift+Tab</code>
+              </td>
+              <td>Move cursor to first unfilled square of next or previous unfilled clue</td>
+            </tr>
+            <tr>
+              <td>
+                <code>Delete</code> or <code>Backspace</code>
+              </td>
+              <td>Clear current cell</td>
+            </tr>
+          </table>
+        </Popup>
+      </div>
+    );
+  }
+
   check(scopeString) {
     this.props.onCheck(scopeString);
   }
@@ -185,6 +251,7 @@ export default class Toolbar extends Component {
         {solved ? null : this.renderRevealMenu()}
         <div className="toolbar--menu reset">{this.renderResetMenu()}</div>
         {this.renderPencil()}
+        {this.renderInfo()}
       </div>
     );
   }
