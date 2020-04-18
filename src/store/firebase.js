b9f2adf0c1eb78e7d0f525b387466a4c91ee42b8
stevenhao <stevenhao@gmail.com>
2020-04-17 21:11:29 -0700

    use staging db in dev

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -5,14 +5,26 @@ import 'firebase/auth';
 let offline = true;
 offline = false;
 
-const config = {
-  apiKey: 'AIzaSyCe4BWm9kbjXFwlZcmq4x8DvLD3TDoinhA',
-  authDomain: 'crosswordsio.firebaseapp.com',
-  databaseURL: 'https://crosswordsio.firebaseio.com',
-  projectId: 'crosswordsio',
-  storageBucket: 'crosswordsio.appspot.com',
-  messagingSenderId: '1021412055058',
+const CONFIGS = {
+  production: {
+    apiKey: 'AIzaSyCe4BWm9kbjXFwlZcmq4x8DvLD3TDoinhA',
+    authDomain: 'crosswordsio.firebaseapp.com',
+    databaseURL: 'https://crosswordsio.firebaseio.com',
+    projectId: 'crosswordsio',
+    storageBucket: 'crosswordsio.appspot.com',
+    messagingSenderId: '1021412055058',
+  },
+  development: {
+    apiKey: 'AIzaSyC4Er27aLKgSK4u2Z8aRfD6mr8AvLPA8tA',
+    authDomain: 'dfac-fa059.firebaseapp.com',
+    databaseURL: 'https://dfac-fa059.firebaseio.com',
+    projectId: 'dfac-fa059',
+    storageBucket: 'dfac-fa059.appspot.com',
+    messagingSenderId: '132564774895',
+    appId: '1:132564774895:web:a3bf48cd38c4df81e8901a',
+  },
 };
+const config = CONFIGS[process.env.NODE_ENV];
 
 firebase.initializeApp(config);
 const db = firebase.database();
