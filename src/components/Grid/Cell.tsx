6d88da5b1f92dba9ec1bc9be47bdf22dff81b59f
audreyli <audrey.li@samsara.com>
2020-05-09 15:34:51 -0700

    Tsx-ify Cell.js
    Was sorta doing this as a proof of concept, but
    went surprisingly more smoothly than I expected, so
    I might tsx-ify some more components.
    
    I also want to reorganize the props maybe

--- src/components/Grid/Cell.tsx
+++ src/components/Grid/Cell.tsx
@@ -1,10 +1,61 @@
-import './css/cell.css';
-
-import React, {Component} from 'react';
-import _ from 'lodash';
+import * as React from 'react';
+import * as _ from 'lodash';
 
 import Emoji from '../common/Emoji';
 import powerups from '../../lib/powerups';
+
+import './css/cell.css';
+
+interface Cursor {
+  id: string;
+  r: number; // Row in puzzle
+  c: number; // Column in puzzle
+  timestamp: number;
+  color: string;
+  active: boolean;
+}
+
+interface Ping extends Cursor {
+  age: number;
+}
+
+interface CellStyle {
+  backgroundColor: string;
+}
+interface CellStyles {
+  selected: CellStyle;
+  highlighted: CellStyle;
+  frozen: CellStyle;
+}
+
+interface Props {
+  value: string;
+  number: number;
+  // Player interactions
+  cursors: Cursor[];
+  pings: Ping[];
+  // Cell states
+  black: boolean;
+  selected: boolean;
+  highlighted: boolean;
+  frozen: boolean;
+  circled: boolean;
+  shaded: boolean;
+  revealed: boolean;
+  bad: boolean;
+  good: boolean;
+  pencil: boolean;
+  referenced: boolean;
+  canFlipColor: boolean;
+  pickupType: keyof typeof powerups;
+  // Styles
+  cellStyle: CellStyles;
+  myColor: string;
+  // Callbacks
+  onClick: () => void;
+  onContextMenu: () => void;
+  onFlipColor: () => void;
+}
 /*
  * Summary of Cell component
  *
@@ -16,9 +67,10 @@ import powerups from '../../lib/powerups';
  * Potential parents:
  * - Grid
  * */
+export default class Cell extends React.Component<Props> {
+  private touchStart: {pageX: number; pageY: number} = {pageX: 0, pageY: 0};
 
-export default class Cell extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
+  shouldComponentUpdate(nextProps: Props) {
     if (!_.isEqual(_.omit(nextProps, 'cursors'), _.omit(this.props, 'cursors'))) return true;
     return !_.isEqual(nextProps.cursors, this.props.cursors);
   }
@@ -46,7 +98,7 @@ export default class Cell extends Component {
     const {pings} = this.props;
     return (
       <div className="cell--pings">
-        {pings.map(({color, active, age}, i) => (
+        {pings.map(({color, active}, i) => (
           <div
             key={i}
             className={`cell--ping ${active ? 'active' : 'inactive'}`}
@@ -96,7 +148,7 @@ export default class Cell extends Component {
     const {pickupType} = this.props;
     if (pickupType) {
       const {icon} = powerups[pickupType];
-      return <Emoji emoji={icon} big={false} className="cell--emoji" />;
+      return <Emoji emoji={icon} big={false} />;
     }
     return null;
   }
