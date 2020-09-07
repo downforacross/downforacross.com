1bf6292ec2c81354d04bc61e911584d95c24c8d3
stevenhao <stevenhao@gmail.com>
2020-09-07 01:50:29 +0000

    update backfill script to be last run one

--- backfills/archive.js
+++ backfills/archive.js
@@ -12,7 +12,7 @@ import _ from 'lodash';
 import AWS from 'aws-sdk';
 import Promise from 'bluebird';
 
-const credentials = new AWS.SharedIniFileCredentials({profile: 'sven'});
+const credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
 AWS.config.credentials = credentials;
 
 const gameWords = 'troff glon yomp bruss jank fress masp smub zint jeft vusk hipt splect sunt phrist dimp bosp zoft yact spluff drid criff jing strod vept luft splob fesp kemp cesk flact thrund clud nund fect swug ust phropt ceft drast fleff scrim omp drap gleck jift jund chand smed noct pron snid vonk trag nept yuft sclack plusk snaff zamp skob glemp besp fress vosk frep jang unt joct thrag plig hect nund sphob blen jisk yasp bisk glaff treb threck plash thrump prash glap thren gaft vesk yeft thrun thomp ont sask trunt blit jemp phrint namp glap prash'.split(
@@ -21,8 +21,10 @@ const gameWords = 'troff glon yomp bruss jank fress masp smub zint jeft vusk hip
 async function go() {
   const SIZE = 50;
   const startTime = Date.now();
+  let work = 0;
   // for (let range = 40000; range < 1000000; range += SIZE) {
-  for (let range = 420000; range < 440000; range += SIZE) {
+  // for (let range = 140000; range > 40000; range -= SIZE) {
+  for (let range = 240000; range > 140000; range -= SIZE) {
     await Promise.map(
       _.range(range, range + SIZE),
       async (gid) => {
@@ -42,20 +44,22 @@ async function go() {
               ACL: 'public-read',
             })
             .promise();
-          // const url = `https://downforacross.s3-us-west-1.amazonaws.com/archive/game/${gid}/events`;
-          // await db.ref(`/game/${gid}/archivedEvents`).set({
-          //   archivedAt: new Date(),
-          //   count: _.keys(history).length,
-          //   url,
-          // });
-          // await db.ref(`/game/${gid}/events`).remove();
+          // console.log('found', gid, word);
+          const url = `https://downforacross.s3-us-west-1.amazonaws.com/archive/game/${gid}/events`;
+          await db.ref(`/game/${gid}-${word}/archivedEvents`).set({
+            archivedAt: new Date(),
+            count: _.keys(history).length,
+            url,
+          });
+          await db.ref(`/game/${gid}-${word}/events`).remove();
+          work += 1;
         });
       },
       {
         concurrency: 50,
       }
     );
-    console.log('done with', range, range + SIZE, (Date.now() - startTime) / 1000 / 60, 'mins');
+    console.log('done with', work, range, range + SIZE, (Date.now() - startTime) / 1000 / 60, 'mins');
   }
 }
 
