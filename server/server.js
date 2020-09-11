acd088a6169f84144d373fed4480662a3e2c9054
stevenhao <stevenhao@gmail.com>
2020-09-10 20:30:28 -0700

    simplify server logic

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const app = express();
 const morgan = require('morgan');
 
 const port = process.env.PORT || 3000;
-const EventEmitter = require('events');
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {pingInterval: 2000, pingTimeout: 5000});
@@ -17,10 +16,6 @@ io.origins('*:*'); // allow CORS for socket.io route
 app.use(cors()); // allow CORS for all express routes
 app.use(morgan('combined'));
 // ============= Database Operations ============
-
-const getEventsKey = (gid) => {
-  return `events_${gid}`;
-};
 
 const MAX_EVENTS = 1e7;
 class GameModel {
@@ -40,11 +35,6 @@ class GameModel {
     await this.client.connect();
     this.connecting = false;
     this.connected = true;
-  }
-
-  disconnect() {
-    // not used
-    return this.client.end();
   }
 
   async getEvents(gid) {
@@ -68,196 +58,65 @@ class GameModel {
   }
 }
 
+// ============= Server Values ===========
+
+// Look for { .sv: 'timestamp' } and relpcae with Date.now()
+function assignTimestamp(event) {
+  if (event && typeof event === 'object') {
+    if (event['.sv'] === 'timestamp') {
+      return Date.now();
+    }
+    const result = event.constructor();
+    for (const key in event) {
+      result[key] = assignTimestamp(event[key]);
+    }
+    return result;
+  }
+  return event;
+}
+
 // ============== Socket Manager ==============
 
-class SocketManager extends EventEmitter {
+class SocketManager {
   constructor(gameModel) {
-    super();
-    this.gameToSocket = new Map();
-    this.socketToGame = new Map(); // Redundancy for sake of correctness
     this.gameModel = gameModel;
   }
 
-  assignTimestamp(event) {
-    if (event && typeof event === 'object') {
-      if (event['.sv'] === 'timestamp') {
-        return Date.now();
-      }
-      const result = event.constructor();
-      for (const key in event) {
-        result[key] = this.assignTimestamp(event[key]);
-      }
-      return result;
-    }
-    return event;
-  }
-
-  // Precondition: gid's game is "hot"
   async addEvent(gid, event) {
-    this.emit('event', gid, event);
-    event = this.assignTimestamp(event);
+    event = assignTimestamp(event);
 
-    // 1. save to DB
-    this.gameModel.addEvent(gid, event);
-    // 2. emit to all live clients
-
-    if (this.gameToSocket.get(gid)) {
-      this.gameToSocket.get(gid).forEach((socket) => {
-        socket.emit('game_event', event);
-      });
-    }
+    await this.gameModel.addEvent(gid, event);
+    io.to(`game-${gid}`).emit('game_event', event);
   }
 
-  getGameConnectionsCount(gid) {
-    const connections = this.gameToSocket.get(gid);
-    return connections ? connections.length : 0;
-  }
-
-  getTotalConnectionsCount() {
-    return this.socketToGame.size;
-  }
-
-  getTotalGamesCount() {
-    return this.gameToSocket.size;
+  getLiveSocketsCount() {
+    return _.keys(io.sockets.sockets).length;
   }
 
   listen() {
     io.on('connection', (socket) => {
-      this.emit('connect', socket.id, socket.handshake.headers['x-real-ip']);
       socket.on('join', async (gid, ack) => {
-        if (this.socketToGame.has(socket)) {
-          throw new Error(
-            `Socket ${socket.id} already joined ${this.socketToGame.get(socket)}, cannot join ${gid}`
-          );
-        }
-
-        if (!this.gameToSocket.get(gid)) {
-          this.gameToSocket.set(gid, []);
-        }
-        this.gameToSocket.get(gid).push(socket);
-        this.socketToGame.set(socket, gid);
-
-        socket.on('game_event', (message, cbk) => {
-          this.addEvent(message.gid, message.event);
-          cbk();
-        });
+        socket.join(`game-${gid}`);
         ack();
-        this.emit('join', gid);
       });
 
-      // Perform the "initial sync"
-      socket.on('sync_all', async (gid, cbk) => {
+      socket.on('sync_all', async (gid, ack) => {
         const events = await this.gameModel.getEvents(gid);
-        cbk(events);
-        this.emit('sync_all', gid, events);
+        ack(events);
       });
 
-      socket.on('disconnect', () => {
-        this.emit('disconnect', socket.id, socket.handshake.headers['x-real-ip']);
-        if (!this.socketToGame.has(socket)) {
-          return;
-        }
-        const gid = this.socketToGame.get(socket);
-        this.socketToGame.delete(socket);
-        if (this.gameToSocket.get(gid)) {
-          _.remove(this.gameToSocket.get(gid), socket);
-          if (!this.gameToSocket.get(gid).size) {
-            this.gameToSocket.delete(gid);
-          }
-        }
+      socket.on('game_event', async (message, ack) => {
+        await this.addEvent(message.gid, message.event);
+        ack();
       });
     });
   }
-
-  emit(type, ...args) {
-    super.emit('*', type, ...args);
-    return super.emit(type, ...args) || super.emit('', ...args);
-  }
 }
-
-// ============== End Socket Manager ==============
-
-// ================== Begin Stats ====================
-
-function getStatsForTimeWindow(socketManager, seconds) {
-  const stats = {
-    windowStart: Date.now(),
-    prevCounts: {
-      gameEvents: 0,
-      activeGames: 0,
-      bytesSent: 0,
-      bytesReceived: 0,
-      connections: 0,
-    },
-    counts: {
-      gameEvents: 0,
-      activeGames: 0,
-      bytesSent: 0,
-      bytesReceived: 0,
-      connections: 0,
-    },
-    activeGids: [],
-  };
-  const activeGames = new Set();
-  socketManager.on('event', (gid, event) => {
-    stats.counts.gameEvents++;
-    const bytes = JSON.stringify(event).length;
-    stats.counts.bytesReceived += bytes;
-    stats.counts.bytesSent += bytes * socketManager.getGameConnectionsCount(gid);
-    if (!activeGames.has(gid)) {
-      activeGames.add(gid);
-      stats.counts.activeGames++;
-      stats.activeGids.push(gid);
-    }
-  });
-
-  socketManager.on('connect', (gid) => {
-    stats.counts.connections += 1;
-  });
-
-  socketManager.on('sync_all', (gid, events) => {
-    const bytes = JSON.stringify(events).length;
-    stats.counts.bytesSent += bytes;
-  });
-
-  setInterval(() => {
-    for (const key in stats.counts) {
-      stats.prevCounts[key] = stats.counts[key];
-      stats.counts[key] = 0;
-    }
-    activeGames.clear();
-    stats.activeGids = [];
-    stats.windowStart = Date.now();
-  }, seconds * 1000);
-  setInterval(() => {
-    stats.percentComplete = (Date.now() - stats.windowStart) / (seconds * 1000);
-  }, 500);
-  return stats;
-}
-
-const STAT_DEFS = [
-  {
-    name: 'minute',
-    secs: 60,
-  },
-  {
-    name: 'hour',
-    secs: 60 * 60,
-  },
-  {
-    name: 'day',
-    secs: 60 * 60 * 24,
-  },
-  {
-    name: 'week',
-    secs: 60 * 60 * 24 * 7,
-  },
-];
 
 // ================== Logging ================
 
 function logAllEvents(socketManager, log) {
-  socketManager.on('*', (event, ...args) => {
+  io.on('*', (event, ...args) => {
     try {
       log(`[${event}]`, _.truncate(JSON.stringify(args), {length: 100}));
     } catch (e) {
@@ -269,29 +128,10 @@ function logAllEvents(socketManager, log) {
 // ================== Main Entrypoint ================
 
 async function runServer() {
-  const serverStartDate = new Date().toString();
   const gameModel = new GameModel();
   const socketManager = new SocketManager(gameModel);
   socketManager.listen();
   logAllEvents(socketManager, console.log);
-
-  const timeWindowStats = STAT_DEFS.map(({name, secs}) => ({
-    name,
-    stats: getStatsForTimeWindow(socketManager, secs),
-  }));
-  app.get('/test', (req, res) => res.send('Hello World!'));
-  app.get('/api/stats', (req, res) => {
-    const liveStats = {
-      serverStartDate,
-      gamesCount: socketManager.getTotalGamesCount(),
-      connectionsCount: socketManager.getTotalConnectionsCount(),
-    };
-    const stats = {
-      liveStats,
-      timeWindowStats,
-    };
-    res.status(200).json(stats);
-  });
   console.log(
     'connecting to database...',
     process.env.PGDATABASE,
