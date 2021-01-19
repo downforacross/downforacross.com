28770bdd4d6ccac7616e0dd50e33bb23ffadc17b
stevenhao <stevenhao@gmail.com>
2021-01-19 01:07:12 -0800

    dummy game rendering

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -3,12 +3,26 @@ import _ from 'lodash';
 import {Helmet} from 'react-helmet';
 import {RouteComponentProps} from 'react-router';
 
-import {RoomEvent, UserPingRoomEvent} from '../shared/roomEvents';
+import {RoomEvent, SetGameRoomEvent, UserPingRoomEvent} from '../shared/roomEvents';
 import {useSocket} from '../sockets/useSocket';
 import {initialRoomState, roomReducer} from '../lib/reducers/room';
 import {emitAsync} from '../sockets/emitAsync';
-import {useMount} from 'react-use';
+import {makeStyles} from '@material-ui/core';
 
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    height: '100%',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: 1,
+    '& iframe': {
+      width: '100%',
+      height: '100%',
+    },
+  },
+});
 function subscribeToRoomEvents(
   socket: SocketIOClient.Socket | undefined,
   rid: string,
@@ -55,6 +69,12 @@ const Room: React.FC<RouteComponentProps<{rid: string}>> = (props) => {
       emitAsync(socket, 'room_event', {rid, event});
     }
   }
+  async function setGame(gid: string) {
+    if (socket) {
+      const event = SetGameRoomEvent(gid);
+      emitAsync(socket, 'room_event', {rid, event});
+    }
+  }
   useEffect(() => {
     setEvents([]);
     const {syncPromise, unsubscribe} = subscribeToRoomEvents(socket, rid, setEvents);
@@ -70,15 +90,28 @@ const Room: React.FC<RouteComponentProps<{rid: string}>> = (props) => {
       window.removeEventListener('keydown', renewActivity);
     };
   }, [rid, socket]);
+  const handleAddGame = () => {
+    const gameLink = window.prompt('Enter game link');
+    const gid = _.last(gameLink?.split('/'));
+    console.log(gid);
+    if (gid && gid.match('[a-z0-9-]{1,15}')) {
+      setGame(gid);
+    }
+  };
+  const classes = useStyles();
+  const currentGame = _.last(roomState.games);
   return (
-    <div
-      style={{
-        width: '100%',
-        height: '100%',
-      }}
-    >
+    <div className={classes.container}>
       <Helmet title={`Room ${rid}`} />
-      <div>{roomState.users.length} users here</div>
+      <div>{roomState.users.length} users</div>
+      {currentGame && <div>Current game: {currentGame.gid}</div>}
+      <div>
+        <button onClick={handleAddGame}>Change game</button>
+      </div>
+      <div className={classes.content}>
+        {currentGame && <iframe src={`/game/${currentGame.gid}`} />}
+        {!currentGame && <div>No game selected!</div>}
+      </div>
     </div>
   );
 };
