2218be3e4bbfa28969a89ca86fe82d662ed3e0e7
stevenhao <stevenhao@gmail.com>
2021-01-18 19:04:12 -0800

    add room reducers

--- src/lib/reducers/room.ts
+++ src/lib/reducers/room.ts
@@ -1,17 +1,41 @@
-import {RoomEventParams, RoomEvent, RoomEventType} from '@shared/roomEvents';
+import {RoomEventParams, RoomEventType} from '../../shared/roomEvents';
 import _ from 'lodash';
 
-interface RoomState {}
+interface RoomState {
+  users: {
+    uid: string;
+    lastPing: number;
+  }[];
+}
+
+export interface RoomEvent<T extends RoomEventType = RoomEventType> {
+  timestamp: number;
+  type: T;
+  params: RoomEventParams[T];
+}
 
 interface RoomReducerFn<T extends RoomEventType = RoomEventType> {
   (room: RoomState, params: RoomEventParams[T], timestamp: number): RoomState;
 }
 
-const userPingReducer: RoomReducerFn<RoomEventType.USER_PING> = (room, params, timestamp) => ({
-  ...room,
-});
+const userPingReducer: RoomReducerFn<RoomEventType.USER_PING> = (room, params, timestamp) => {
+  const newUsers = [
+    {
+      uid: params.uid,
+      lastPing: timestamp,
+    },
+    ...room.users.filter((user) => user.uid !== params.uid),
+  ];
+  return {
+    ...room,
+    users: newUsers,
+  };
+};
 
-export const reduce = (room: RoomState, event: RoomEvent, options = {}): RoomState => {
+export const initialRoomState: RoomState = {
+  users: [],
+};
+export const roomReducer = (room: RoomState, event: RoomEvent, options = {}): RoomState => {
   try {
     switch (event.type) {
       case RoomEventType.USER_PING:
