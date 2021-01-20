92e8c73526687a69e43d1ee5f05808e66547265d
stevenhao <stevenhao@gmail.com>
2021-01-19 17:36:07 -0800

    rework GameState to have a separate game?: Game field

--- src/lib/reducers/gameV2.ts
+++ src/lib/reducers/gameV2.ts
@@ -6,7 +6,7 @@ import {
   isUserPingGameEvent,
 } from '../../shared/gameEvents';
 import _ from 'lodash';
-import {InfoJson} from '../../shared/types';
+import {CluesJson, InfoJson} from '../../shared/types';
 import {GridData} from '../../components/Grid/types';
 
 /*
@@ -33,11 +33,17 @@ from ./game.js -- which we are seeking to replace by gameV2.ts
       // themeColor = GREENISH,
     } = params.game;
     */
-export interface GameState {
-  loaded: boolean;
+
+// TODO move GameJson to a shared type, probably (closer to where InfoJson / CluesJson are declared)
+export interface GameJson {
   info: InfoJson;
   grid: GridData;
   solution: string[][];
+  clues: CluesJson;
+}
+export interface GameState {
+  loaded: boolean;
+  game: null | GameJson;
   // a lot of the previous implementation of GameState is suboptimal
   // TODO - add scoreboard for battle
   // TODO - add a list of users + track their activity
@@ -47,31 +53,31 @@ interface GameReducerFn<T extends GameEventType = GameEventType> {
   (game: GameState, params: GameEventParams[T], timestamp: number): GameState;
 }
 
-const userPingReducer: GameReducerFn<GameEventType.USER_PING> = (game, params, timestamp) => {
-  return game;
+const userPingReducer: GameReducerFn<GameEventType.USER_PING> = (gameState, params, timestamp) => {
+  return gameState;
 };
 
-const initializeGameReducer: GameReducerFn<GameEventType.INITIALIZE_GAME> = (game, params) => ({
-  ...game,
+const initializeGameReducer: GameReducerFn<GameEventType.INITIALIZE_GAME> = (gameState, params) => ({
   loaded: true,
-  pid: params.pid,
-  ...params.game,
+  game: {
+    ...initialGameState.game,
+    pid: params.pid,
+    ...params.game,
+  },
 });
 
 export const initialGameState: GameState = {
   loaded: false,
-  info: {
-    title: '',
-    author: '',
-    description: '',
-    copyright: '',
-  },
-  grid: [[{}]],
-  solution: [['A']],
+  game: null,
 };
 
 export const gameReducer = (game: GameState, event: GameEvent, options = {}): GameState => {
   try {
+    if (!game.loaded && !isInitializeGameEvent(event)) {
+      console.error('Error handling event, game not yet loaded', event);
+      return game;
+    }
+
     if (isUserPingGameEvent(event)) {
       return userPingReducer(game, event.params, event.timestamp);
     } else if (isInitializeGameEvent(event)) {
