65bb919c23d5ac00862e4cace0c6cea50ab22e78
stevenhao <stevenhao@gmail.com>
2021-01-24 13:20:09 -0800

    update with example data & invocation

--- server/sql/adhoc/load_puzzles_1_24_21.sql
+++ server/sql/adhoc/load_puzzles_1_24_21.sql
@@ -1,5 +1,14 @@
 -- Load puzzles from a csv dump.
 -- see backfills/dumpPuzzles.js for how to create such a csv dump.
+
+-- ***example data***
+-- uid,pid,is_public,uploaded_at,content
+-- 1,1,false,2021-01-24T21:02:56.628Z,
+-- 2,2,true,2021-01-24T21:02:56.628Z, 
+
+-- ***invocation***
+-- cat load_puzzles_1_24_21.sql data/puzzles.csv | psql
+
 COPY PUBLIC.PUZZLES(uid, pid, is_public, uploaded_at, content)
 FROM STDIN
 DELIMITER ','
