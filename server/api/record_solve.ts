1c1ee1c2115fce5c742dd649a308466813ca84fd
stevenhao <stevenhao@gmail.com>
2021-01-25 11:54:16 -0800

    use express error handling correctly in router to avoid uncaught error in promise

--- server/api/record_solve.ts
+++ server/api/record_solve.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import {nextTick} from 'process';
 import {RecordSolveRequest, RecordSolveResponse} from '../../src/shared/types';
 import {recordSolve} from '../model/puzzle';
 
 const router = express.Router();
 
-router.post<{pid: string}, RecordSolveResponse, RecordSolveRequest>('/:pid', async (req, res) => {
+router.post<{pid: string}, RecordSolveResponse, RecordSolveRequest>('/:pid', async (req, res, next) => {
   const recordSolveRequest = req.body;
-  await recordSolve(req.params.pid, recordSolveRequest.gid, recordSolveRequest.time_to_solve);
-  res.json({});
+  try {
+    await recordSolve(req.params.pid, recordSolveRequest.gid, recordSolveRequest.time_to_solve);
+    res.json({});
+  } catch (e) {
+    next(e);
+  }
 });
 
 export default router;
