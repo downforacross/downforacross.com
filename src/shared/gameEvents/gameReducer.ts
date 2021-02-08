f0072bcdd240935293815609ab27468d56718bb6
stevenhao <stevenhao@gmail.com>
2021-02-07 20:58:29 -0800

    warn not implemented

--- src/shared/gameEvents/gameReducer.ts
+++ src/shared/gameEvents/gameReducer.ts
@@ -2,8 +2,12 @@ import allEventDefs from './allEventDefs';
 import {GameEvent} from './types/GameEvent';
 import {GameState} from './types/GameState';
 
-export default (state: GameState, event: GameEvent): GameState =>
-  allEventDefs[event.type]?.reducer(state, event.params) ?? state; // TODO fix ts here
+export default (state: GameState, event: GameEvent): GameState => {
+  if (!(event.type in allEventDefs)) {
+    console.warn(`Game event not implemented: ${event.type}`);
+  }
+  return allEventDefs[event.type]?.reducer(state, event.params) ?? state; // TODO fix ts here
+};
 
 export const initialState: GameState = {
   loaded: false,
