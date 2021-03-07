1fbc0d9e3108f5373e176f8e1ba901674901f821
stevenhao <stevenhao@gmail.com>
2021-03-06 18:19:41 -0800

    redesign log

--- src/components/ModernArt/GameStateDisplayer/Log.tsx
+++ src/components/ModernArt/GameStateDisplayer/Log.tsx
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import {useUpdateEffect} from 'react-use';
 import {makeStyles} from '@material-ui/core';
 import {LogMessage} from '../events/types';
 
 export const Log: React.FC<{log: LogMessage[]}> = (props) => {
   const classes = useStyles();
+  const logRef = useRef<HTMLPreElement>(null);
+  const [fullyScrolled, setIsFullyScrolled] = useState(false);
+  const checkScroll = () => {
+    if (!logRef.current) return;
+    setIsFullyScrolled(
+      logRef.current.scrollTop >= logRef.current.scrollHeight - logRef.current.getBoundingClientRect().height
+    );
+  };
+  useUpdateEffect(() => {
+    if (!logRef.current) return;
+    if (fullyScrolled) {
+      logRef.current.scrollTop = logRef.current.scrollHeight;
+    }
+  }, [props.log]);
   return (
-    <pre className={classes.log}>
-      {props.log.map((message) => (
-        <div className={classes.logMessage}>
-          {'>'} {message.hhmm} {message.text}
-        </div>
-      ))}
-    </pre>
+    <div className={classes.container}>
+      <pre ref={logRef} className={classes.log} onScroll={checkScroll}>
+        {props.log.map((message) => (
+          <div className={classes.logMessage}>
+            {'>'} {message.hhmm} {message.text}
+          </div>
+        ))}
+      </pre>
+      {!fullyScrolled && <div className={classes.notFullyScrolledMessage}>Scroll for more messages</div>}
+    </div>
   );
 };
 
 const useStyles = makeStyles({
-  log: {},
+  container: {
+    position: 'relative',
+  },
+  log: {
+    overflow: 'auto',
+    maxHeight: 200,
+  },
   logMessage: {},
+  notFullyScrolledMessage: {
+    position: 'absolute',
+    bottom: 0,
+    background: 'rgba(0, 0, 0, 70%)',
+    color: 'white',
+    width: '100%',
+    padding: 4,
+    textAlign: 'center',
+  },
 });
