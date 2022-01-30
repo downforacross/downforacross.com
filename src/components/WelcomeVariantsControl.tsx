5b3651bac7ee755e98d7cbf43773230b3e1865fe
stevenhao <stevenhao@gmail.com>
2022-01-30 02:02:49 -0500

    info message

--- src/components/WelcomeVariantsControl.tsx
+++ src/components/WelcomeVariantsControl.tsx
@@ -2,11 +2,39 @@ import {makeStyles} from '@material-ui/core';
 import React from 'react';
 import {Link} from 'react-router-dom';
 import clsx from 'clsx';
+// @ts-ignore
+import swal from '@sweetalert/with-react';
 
 export const WelcomeVariantsControl: React.FC<{
   fencing?: boolean;
 }> = (props) => {
   const classes = useStyles();
+  const showFencingInfo = () => {
+    swal({
+      title: 'downforacross.com',
+      icon: 'info',
+      content: (
+        <div className="swal-text swal-text--no-margin">
+          <p>
+            Fencing is a variant of Down for a Cross where you can race to complete a crossword against
+            friends in real time.
+            <br />
+            <br />
+            Correctly fill in cells correctly before the other team to unlock more clues and explore the grid.
+            <br />
+            <br />
+            <span style={{fontSize: '75%', color: 'gray'}}>
+              Join the&nbsp;
+              <a href="https://discord.gg/KjPHFw8" target="_blank" rel="noreferrer">
+                community Discord
+              </a>
+              &nbsp;for more discussion.
+            </span>
+          </p>
+        </div>
+      ),
+    });
+  };
   return (
     <div className={classes.container}>
       <span className={classes.title}>Variants</span>
@@ -19,15 +47,20 @@ export const WelcomeVariantsControl: React.FC<{
           Normal
         </span>
       </Link>
-      <Link to="/fencing">
-        <span
-          className={clsx(classes.option, {
-            selected: !!props.fencing,
-          })}
-        >
-          Fencing
+      <span>
+        <Link to="/fencing">
+          <span
+            className={clsx(classes.option, {
+              selected: !!props.fencing,
+            })}
+          >
+            Fencing
+          </span>
+        </Link>
+        <span className="nav--info" onClick={showFencingInfo}>
+          <i className="fa fa-info-circle" />
         </span>
-      </Link>
+      </span>
     </div>
   );
 };
