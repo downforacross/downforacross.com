2aa245a5866604bdfe34622bdefb949b628b703a
Brian Liu <b_liu72@yahoo.com>
2023-01-12 22:12:29 -0500

    Add dark mode preference with localStorage (#259)
    * Add dark mode preference with localStorage
    
    * Remove swp file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import classnames from 'classnames';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {isMobile} from './lib/jsUtils';
@@ -24,16 +25,38 @@ import GlobalContext from './lib/GlobalContext';
 import './style.css';
 import './dark.css';
 
+const darkModeLocalStorageKey = 'dark_mode_preference';
+
 const Root = () => {
-  const initialValue = window.location.search.indexOf('dark') !== -1;
-  const [darkMode, setDarkMode] = React.useState(initialValue);
+  const urlDarkMode = window.location.search.indexOf('dark') !== -1;
+  const savedDarkModePreference = (localStorage && localStorage.getItem(darkModeLocalStorageKey)) || '0';
+  const [darkModePreference, setDarkModePreference] = React.useState(
+    urlDarkMode ? '1' : savedDarkModePreference
+  );
+
   const toggleMolesterMoons = () => {
-    setDarkMode(!darkMode);
+    let newDarkModePreference;
+    switch (darkModePreference) {
+      case '0':
+        newDarkModePreference = '1';
+        break;
+      case '1':
+        newDarkModePreference = '2';
+        break;
+      case '2':
+      default:
+        newDarkModePreference = '0';
+    }
+    localStorage && localStorage.setItem(darkModeLocalStorageKey, newDarkModePreference);
+    setDarkModePreference(newDarkModePreference);
   };
+
+  const systemDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const darkMode = darkModePreference === '2' ? systemDarkMode : darkModePreference === '1';
 
   return (
     <Router>
-      <GlobalContext.Provider value={{toggleMolesterMoons}}>
+      <GlobalContext.Provider value={{toggleMolesterMoons, darkModePreference}}>
         <div className={classnames('router-wrapper', {mobile: isMobile(), dark: darkMode})}>
           <Switch>
             <Route exact path="/" component={Welcome} />
