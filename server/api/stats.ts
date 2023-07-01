0ddba04a486d8cc4424c49464707d0077c80bf15
stevenhao <stevenhao@gmail.com>
2023-06-30 20:52:02 -0400

    post instead of get for stats endpoints

--- server/api/stats.ts
+++ server/api/stats.ts
@@ -40,40 +40,43 @@ export function computePuzzleStats(puzzle_solves: SolvedPuzzleType[]): PuzzleSum
   return stats.sort((a, b) => a.size.localeCompare(b.size));
 }
 
-router.get<{gids: string[]}, ListPuzzleStatsResponse, ListPuzzleStatsRequest>('/', async (req, res, next) => {
-  const gids = req.query.gids;
-  const startTime = Date.now();
-  if (!Array.isArray(gids) || !_.every(gids, (it) => typeof it === 'string')) {
-    next(_.assign(new Error('gids are invalid'), {statusCode: 400}));
-  }
-  const puzzleSolves = await getPuzzleSolves(gids as string[]);
-  const puzzleStats = computePuzzleStats(puzzleSolves);
-  const stats = puzzleStats.map((stat) => ({
-    size: stat.size,
-    nPuzzlesSolved: stat.n_puzzles_solved,
-    avgSolveTime: stat.avg_solve_time,
-    bestSolveTime: stat.best_solve_time,
-    bestSolveTimeGameId: stat.best_solve_time_game,
-    avgCheckedSquareCount: stat.avg_checked_square_count,
-    avgRevealedSquareCount: stat.avg_revealed_square_count,
-  }));
-  const history = puzzleSolves.map((solve) => ({
-    puzzleId: solve.pid,
-    gameId: solve.gid,
-    title: solve.title,
-    size: solve.size,
-    dateSolved: solve.solved_time.format('YYYY-MM-DD'),
-    solveTime: solve.time_taken_to_solve,
-    checkedSquareCount: solve.checked_squares_count,
-    revealedSquareCount: solve.revealed_squares_count,
-  }));
+router.post<{gids: string[]}, ListPuzzleStatsResponse, ListPuzzleStatsRequest>(
+  '/',
+  async (req, res, next) => {
+    const gids = req.body.gids;
+    const startTime = Date.now();
+    if (!Array.isArray(gids) || !_.every(gids, (it) => typeof it === 'string')) {
+      next(_.assign(new Error('gids are invalid'), {statusCode: 400}));
+    }
+    const puzzleSolves = await getPuzzleSolves(gids as string[]);
+    const puzzleStats = computePuzzleStats(puzzleSolves);
+    const stats = puzzleStats.map((stat) => ({
+      size: stat.size,
+      nPuzzlesSolved: stat.n_puzzles_solved,
+      avgSolveTime: stat.avg_solve_time,
+      bestSolveTime: stat.best_solve_time,
+      bestSolveTimeGameId: stat.best_solve_time_game,
+      avgCheckedSquareCount: stat.avg_checked_square_count,
+      avgRevealedSquareCount: stat.avg_revealed_square_count,
+    }));
+    const history = puzzleSolves.map((solve) => ({
+      puzzleId: solve.pid,
+      gameId: solve.gid,
+      title: solve.title,
+      size: solve.size,
+      dateSolved: solve.solved_time.format('YYYY-MM-DD'),
+      solveTime: solve.time_taken_to_solve,
+      checkedSquareCount: solve.checked_squares_count,
+      revealedSquareCount: solve.revealed_squares_count,
+    }));
 
-  const ms = Date.now() - startTime;
-  console.log(`overall /api/stats took ${ms}ms for ${puzzleSolves.length} solves`);
-  res.json({
-    stats,
-    history,
-  });
-});
+    const ms = Date.now() - startTime;
+    console.log(`overall /api/stats took ${ms}ms for ${puzzleSolves.length} solves`);
+    res.json({
+      stats,
+      history,
+    });
+  }
+);
 
 export default router;
--- src/api/stats.ts
+++ src/api/stats.ts
@@ -1,11 +1,18 @@
 // ========== GET /api/stats ============
 
-import qs from 'qs';
 import {SERVER_URL} from './constants';
 import {ListPuzzleStatsRequest, ListPuzzleStatsResponse} from '../shared/types';
 
 export async function fetchStats(query: ListPuzzleStatsRequest) {
-  const resp = await fetch(`${SERVER_URL}/api/stats?${qs.stringify(query)}`);
+  // const resp = await fetch(`${SERVER_URL}/api/stats`, ?${qs.stringify(query)}`);
+  const resp = await fetch(`${SERVER_URL}/api/stats`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(query),
+  });
+
   const allStats: ListPuzzleStatsResponse = await resp.json();
   return allStats;
 }
