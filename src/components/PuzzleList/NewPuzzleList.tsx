bfc7c071e7ac96814cc0cdbe82282039baa59123
David Lakata <davidlakata@gmail.com>
2023-07-04 11:10:53 -0400

    Group existing duplicate puzzles in UI

--- src/components/PuzzleList/NewPuzzleList.tsx
+++ src/components/PuzzleList/NewPuzzleList.tsx
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import React, {useEffect, useRef, useState} from 'react';
-import {PuzzleJson, PuzzleStatsJson, ListPuzzleRequestFilters} from '../../shared/types';
+import {PuzzleJson, PuzzleStatsJson, ListPuzzleRequestFilters, ListedPuzzle} from '../../shared/types';
 import {fetchPuzzleList} from '../../api/puzzle_list';
 import './css/puzzleList.css';
 import Entry, {EntryProps} from './Entry';
@@ -20,19 +20,46 @@ interface NewPuzzleListProps {
   fencing?: boolean;
 }
 
+const summarizePuzzles = (
+  puzzles: ListedPuzzle[],
+  puzzleStatuses: PuzzleStatuses
+): {
+  representativePuzzle: ListedPuzzle;
+  puzzlesToRender: ListedPuzzle[];
+  aggregateStatus: 'solved' | 'started' | undefined;
+} => {
+  let representativePuzzle;
+  let puzzlesToRender;
+
+  const solvedPuzzles = puzzles.filter((p) => puzzleStatuses[p.pid] === 'solved');
+  const startedPuzzles = puzzles.filter((p) => puzzleStatuses[p.pid] === 'started');
+  let aggregateStatus: 'solved' | 'started' | undefined;
+  if (solvedPuzzles.length > 0) {
+    // if the user has solved a puzzle instance, use that as the representative puzzle
+    representativePuzzle = solvedPuzzles[0];
+    puzzlesToRender = puzzles.filter((p) => puzzleStatuses[p.pid]);
+    aggregateStatus = 'solved';
+  } else if (startedPuzzles.length > 0) {
+    // or, if the user has started a puzzle instance, use that as the representative puzzle
+    representativePuzzle = startedPuzzles[0];
+    puzzlesToRender = puzzles.filter((p) => puzzleStatuses[p.pid]);
+    aggregateStatus = 'started';
+  } else {
+    // otherwise, use the most commonly solved instance
+    const mostSolvedPuzzle = _.maxBy(puzzles, (p) => p.stats.numSolves)!;
+    representativePuzzle = mostSolvedPuzzle;
+    puzzlesToRender = [mostSolvedPuzzle];
+  }
+  return {representativePuzzle, puzzlesToRender, aggregateStatus};
+};
+
 const NewPuzzleList: React.FC<NewPuzzleListProps> = (props) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [fullyLoaded, setFullyLoaded] = useState<boolean>(false);
   const [page, setPage] = useState<number>(0);
   const pageSize = 50;
-  const [puzzles, setPuzzles] = useState<
-    {
-      pid: string;
-      content: PuzzleJson;
-      stats: PuzzleStatsJson;
-    }[]
-  >([]);
+  const [puzzles, setPuzzles] = useState<ListedPuzzle[]>([]);
   const fullyScrolled = (): boolean => {
     if (!containerRef.current) return false;
     const {scrollTop, scrollHeight, clientHeight} = containerRef.current;
@@ -42,14 +69,7 @@ const NewPuzzleList: React.FC<NewPuzzleListProps> = (props) => {
 
   const fetchMore = React.useCallback(
     _.throttle(
-      async (
-        currentPuzzles: {
-          pid: string;
-          content: PuzzleJson;
-          stats: PuzzleStatsJson;
-        }[],
-        currentPage: number
-      ) => {
+      async (currentPuzzles: ListedPuzzle[], currentPage: number) => {
         if (loading) return;
         setLoading(true);
         const nextPage = await fetchPuzzleList({page: currentPage, pageSize, filter: props.filter});
@@ -82,27 +102,54 @@ const NewPuzzleList: React.FC<NewPuzzleListProps> = (props) => {
 
   const puzzleData: {
     entryProps: EntryProps;
-  }[] = puzzles
-    .map((puzzle) => ({
-      entryProps: {
-        info: {
-          type: puzzle.content.info.type!, // XXX not the best form
+  }[] = _.chain(puzzles)
+    .groupBy((p) => p.puzzleHash)
+    .map((puzzles, _hash) => {
+      const {representativePuzzle, aggregateStatus, puzzlesToRender} = summarizePuzzles(
+        puzzles,
+        props.puzzleStatuses
+      );
+      return {
+        entryProps: {
+          info: {
+            type: representativePuzzle.content.info.type!, // XXX not the best form
+          },
+          status: aggregateStatus,
+          representativeId: representativePuzzle.pid,
+          aggregateStatus: aggregateStatus,
+          numSolves: representativePuzzle.stats.numSolves || 0,
+          title: representativePuzzle.content.info.title,
+          author: representativePuzzle.content.info.author,
+          fencing: props.fencing,
+          puzzles: puzzlesToRender.map((puzzle) => ({
+            pid: puzzle.pid,
+            status: props.puzzleStatuses[puzzle.pid],
+          })),
         },
-        title: puzzle.content.info.title,
-        author: puzzle.content.info.author,
-        pid: puzzle.pid,
-        stats: puzzle.stats,
-        status: props.puzzleStatuses[puzzle.pid],
-        fencing: props.fencing,
-      },
-    }))
+      };
+    })
     .filter((data) => {
-      const mappedStatus = {
-        undefined: 'New' as const,
+      const statusMapping = {
         solved: 'Complete' as const,
         started: 'In progress' as const,
-      }[data.entryProps.status];
+      };
+      let mappedStatus: 'New' | 'Complete' | 'In progress';
+      if (data.entryProps.status) {
+        mappedStatus = statusMapping[data.entryProps.status];
+      } else {
+        mappedStatus = 'New';
+      }
       return props.statusFilter[mappedStatus];
+    })
+    .value()
+    .sort((p1, p2) => {
+      // we sort the results by pid so that scrolling doesn't cause the results to shuffle
+      const p1Id = parseInt(p1.entryProps.representativeId);
+      const p2Id = parseInt(p2.entryProps.representativeId);
+      if (p1Id && p2Id) {
+        return p2Id - p1Id;
+      }
+      return p2.entryProps.representativeId.localeCompare(p1.entryProps.representativeId);
     });
   return (
     <div
