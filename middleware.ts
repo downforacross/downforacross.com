2f1688bb3e02b6b985172fdc0a8b81722108b7ee
shintyl <tylerjshin@gmail.com>
2023-07-25 03:06:51 -0400

    Include oEmbed headers in game URL responses

--- middleware.ts
+++ middleware.ts
@@ -2,22 +2,34 @@ function isAsset(url) {
     return /\.(png|jpe?g|gif|css|js|svg|ico|map|json)$/i.test(url.pathname)
 }
 
+function isGameUrl(url) {
+    const split = url.pathname.split('/')
+    return split.length >= 3 && split[2] === 'game'
+}
+
 export default function middleware(req: Request) {
     const url = new URL(req.url)
 
-    if (isAsset(url)) {
+    if (isAsset(url) || !isGameUrl(url)) {
         return new Response(null, {
             headers: { 'x-middleware-next': '1' },
         })
     }
 
-    const headers = new Headers({ 'x-custom-1': 'value-1' })
-    headers.set('x-custom-2', 'value-2')
+    const oEmbedEndpoint = `${url.origin}'/api/oembed?url=${encodeURI(url.toString())}`
+    const linkHeaderContent = [
+        `<${oEmbedEndpoint}>`,
+        'rel="alternate"',
+        'type="application/json+oembed"',
+        'title="placeholder"'
+    ]
+
+    console.log(linkHeaderContent)
 
     return new Response(null, {
         headers: {
             'x-middleware-next': '1',
-            ...Object.fromEntries(headers),
+            'Link': linkHeaderContent.join('; '),
         },
     })
 }
--- server/api/router.ts
+++ server/api/router.ts
@@ -4,6 +4,7 @@ import puzzleRouter from './puzzle';
 import gameRouter from './game';
 import recordSolveRouter from './record_solve';
 import statsRouter from './stats';
+import oEmbedRouter from './oembed'
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use('/puzzle', puzzleRouter);
 router.use('/game', gameRouter);
 router.use('/record_solve', recordSolveRouter);
 router.use('/stats', statsRouter);
+router.use('/oembed', oEmbedRouter);
 
 export default router;
