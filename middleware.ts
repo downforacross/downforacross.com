7c43d6ff6fa36dc5095c10cc84d9208988ed630c
shintyl <tylerjshin@gmail.com>
2023-08-09 18:57:49 -0400

    SERVER_URL doesn't work in middleware :/

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
 import { islinkExpanderBot } from "./utils/link_preview_util"
-import { SERVER_URL } from "./src/api/constants"
 
 function isAsset(url) {
     return /\.(png|jpe?g|gif|css|js|svg|ico|map|json)$/i.test(url.pathname)
@@ -13,16 +12,17 @@ function isGameUrl(url) {
 export default function middleware(req: Request) {
     const url = new URL(req.url)
 
-    // if (isAsset(url) || !isGameUrl(url) || !islinkExpanderBot(req.headers.get('user-agent') as string)) {
-    //     return new Response(null, {
-    //         headers: { 'x-middleware-next': '1' },
-    //     })
-    // }
-
-    return new Response(SERVER_URL, {
-        // status: 307,
-        // headers: {
-        //     'Location': `https://www.google.com?url=${url}`, // temp url for testing, needs to be replaced by api.foracross.com
-        // },
+    if (isAsset(url) || !isGameUrl(url) || !islinkExpanderBot(req.headers.get('user-agent') as string)) {
+        return new Response(null, {
+            headers: { 'x-middleware-next': '1' },
+        })
+    }
+    
+    // crawled by link expander bot, so redirect to link preview endpoint for this game URL
+    return new Response(null, { 
+        status: 307,
+        headers: {
+            'Location': `https://api.foracross.com/api/link_preview?url=${url}` // vercel middleware can't really be tested in dev so just hardcoding to prod endpoint
+        },
     })
 }   
