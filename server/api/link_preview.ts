13063bb4798602f8c52e112671fbf5e7d9ecf376
stevenhao <stevenhao@gmail.com>
2023-08-21 11:02:49 -0700

    support /play in link preview

--- server/api/link_preview.ts
+++ server/api/link_preview.ts
@@ -1,61 +1,71 @@
 import express from 'express';
-import _ from 'lodash'
+import _ from 'lodash';
 
-import { InfoJson } from '../../src/shared/types';
-import { getGameInfo } from '../model/game';
-import { islinkExpanderBot, isFBMessengerCrawler } from "../../utils/link_preview_util"
+import {InfoJson} from '../../src/shared/types';
+import {getGameInfo} from '../model/game';
+import {islinkExpanderBot, isFBMessengerCrawler} from '../../utils/link_preview_util';
+import {getPuzzleInfo} from '../model/puzzle';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    console.log('got req', req.headers, req.body);
-    let gameUrl;
+  console.log('got req', req.headers, req.body);
+  let url;
 
-    try {
-        gameUrl = new URL(req.query.url as string);
-    } catch (_) {
-        res.sendStatus(400);
-        return
-    }
+  try {
+    url = new URL(req.query.url as string);
+  } catch (_) {
+    res.sendStatus(400);
+    return;
+  }
 
-    const gid = gameUrl.pathname.split('/')[3]
-    const info = await getGameInfo(gid) as InfoJson;
+  let info;
+  if (url.pathname.split('/')[2] === 'game') {
+    const gid = url.pathname.split('/')[3];
+    info = (await getGameInfo(gid)) as InfoJson;
+  } else {
+    const pid = url.pathname.split('/')[3];
+    info = (await getPuzzleInfo(pid)) as InfoJson;
+  }
 
-    if (_.isEmpty(info)) {
-        res.sendStatus(404);
-        return
-    }
+  if (_.isEmpty(info)) {
+    res.sendStatus(404);
+    return;
+  }
 
-    const ua = req.headers['user-agent'] as string
+  const ua = req.headers['user-agent'] as string;
 
-    if (!islinkExpanderBot(ua)) { // In case a human accesses this endpoint
-        res.redirect(gameUrl.href)
-        return
-    }
+  if (!islinkExpanderBot(ua)) {
+    // In case a human accesses this endpoint
+    res.redirect(url.href);
+    return;
+  }
 
-    // OGP doesn't support an author property, so we need to delegate to the oEmbed endpoint
-    const oembedEndpointUrl = `${req.protocol}://${req.get('host')}/api/oembed?author=${encodeURIComponent(info.author)}`
+  // OGP doesn't support an author property, so we need to delegate to the oEmbed endpoint
+  const oembedEndpointUrl = `${req.protocol}://${req.get('host')}/api/oembed?author=${encodeURIComponent(
+    info.author
+  )}`;
 
-    // Messenger only supports title + thumbnail, so cram everything into the title property if Messenger
-    const titlePropContent = isFBMessengerCrawler(ua)
-        ? [info.title, info.author, info.description].filter(Boolean).join(' | ')
-        : info.title
+  // Messenger only supports title + thumbnail, so cram everything into the title property if Messenger
+  const titlePropContent = isFBMessengerCrawler(ua)
+    ? [info.title, info.author, info.description].filter(Boolean).join(' | ')
+    : info.title;
 
-    // https://ogp.me
-    res.send(String.raw`
+  // https://ogp.me
+  res.send(String.raw`
         <html prefix="og: https://ogp.me/ns/website#">
             <head>
                 <title>${titlePropContent}</title>
                 <meta property="og:title" content="${titlePropContent}" />
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content="${gameUrl.href}" />
+                <meta property="og:url" content="${url.href}" />
                 <meta property="og:description" content="${info.description}" />
                 <meta property="og:site_name" content="downforacross.com" />
                 <link type="application/json+oembed" href=${oembedEndpointUrl} />
                 <meta name="theme-color" content="#6aa9f4">
             </head>
         </html>
-    `)
+    `);
 });
 
 export default router;
