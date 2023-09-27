b7f80cc897cf616f6c1402ca6bd216b1fe3c779d
Mitchell Vitez <mitchell@vitez.me>
2023-09-27 14:22:44 -0700

    fix sorting to use comparison function

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -331,7 +331,7 @@ export default class Player extends Component {
       <div style={{marginTop: 24}}>
         <strong>Squares filled by user</strong>
         {Object.entries(counts)
-          .sort(([, count]) => parseInt(count, 10))
+          .sort(([, countA], [, countB]) => countA - countB)
           .map(([userId, count]) => (
             <div style={{color: this.props.users[userId]?.color}}>
               {this.props.users[userId]?.displayName} - {count}
